7f9d90904c00e86713c752bf8d7e6a161b773761
tecnico <tecnico@adrinfor.com>
2019-04-16 13:58:28 +0200

    commit inicial

---
+++ js/ratio.js
@@ -0,0 +1,5 @@
+const FACTOR = 1.12986;
+
+var euros = prompt("Introduzca la cantidad que euros a convertir en dólares: ");
+
+document.write(euros + " euros son " + (euros * FACTOR) + " dólares.");
